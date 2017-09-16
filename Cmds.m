e10dd1608b2ad49d38ccd5f8c52085ae5ae80252
shihchengyen <30923143+shihchengyen@users.noreply.github.com>
2017-09-16 16:58:09 +0800

    Added example of using a nptgroup object.

--- Cmds.m
+++ Cmds.m
@@ -6,74 +6,30 @@ rplsplit('auto','SaveLevels',2,'SkipRaw');
 ProcessLevel(vmlfp,'Levels','session','save')
 
 % change directory to one of the arrays, e.g. array01
-cd(array01)
+cd('array01')
 % create objects
 cd('channel001')
 vp1 = vmlfp('auto');
+
+% look at Channel 1 trial-by-trial
+InspectGUI(vp1)
+% increase the pre-trial window to 1000 ms
+InspectGUI(vp1,'PreTrial',1000)
+% plot all trials as a heat map
+plot(vp1)
+% normalize each trial before plotting heatmap
+plot(vp1,'NormalizeTrial')
+
 cd('../channel002');
 vp2 = vmlfp('auto');
-cd('../channel003');
-vp3 = vmlfp('auto');
-cd('../channel004');
-vp4 = vmlfp('auto');
-cd('../channel005');
-vp5 = vmlfp('auto');
-cd('../channel006');
-vp6 = vmlfp('auto');
-cd('../channel007');
-vp7 = vmlfp('auto');
-cd('../channel008');
-vp8 = vmlfp('auto');
-cd('../channel009');
-vp9 = vmlfp('auto');
-cd('../channel010');
-vp10 = vmlfp('auto');
-cd('../channel011');
-vp11 = vmlfp('auto');
-cd('../channel012');
-vp12 = vmlfp('auto');
-cd('../channel013');
-vp13 = vmlfp('auto');
-cd('../channel014');
-vp14 = vmlfp('auto');
-cd('../channel015');
-vp15 = vmlfp('auto');
-cd('../channel016');
-vp16 = vmlfp('auto');
-cd('../channel017');
-vp17 = vmlfp('auto');
-cd('../channel018');
-vp18 = vmlfp('auto');
-cd('../channel019');
-vp19 = vmlfp('auto');
-cd('../channel020');
-vp20 = vmlfp('auto');
-cd('../channel021');
-vp21 = vmlfp('auto');
-cd('../channel022');
-vp22 = vmlfp('auto');
-cd('../channel023');
-vp23 = vmlfp('auto');
-cd('../channel024');
-vp24 = vmlfp('auto');
-cd('../channel025');
-vp25 = vmlfp('auto');
-cd('../channel026');
-vp26 = vmlfp('auto');
-cd('../channel027');
-vp27 = vmlfp('auto');
-cd('../channel028');
-vp28 = vmlfp('auto');
-cd('../channel029');
-vp29 = vmlfp('auto');
-cd('../channel030');
-vp30 = vmlfp('auto');
-cd('../channel031');
-vp31 = vmlfp('auto');
-cd('../channel032');
-vp32 = vmlfp('auto');
 
-InspectGUI(vp1,'addObjs',{vp2,vp3,vp4,vp5,vp6,vp7,vp8,vp9,vp10,vp11,vp12, ...
-	vp13,vp14,vp15,vp16,vp17,vp18,vp19,vp20,vp21,vp22,vp23,vp24,vp25,vp26,...
-	vp27,vp28,vp29,vp30,vp31,vp32})
-	
+% look at Channels 1 and 2 together, with the subplots arranged as [2 1]
+InspectGUI(vp1,'addObjs',{vp2},'SP',[2 1])
+
+% look at all channels on array 1
+% change to array01 directory
+cd ..
+% create nptgroup object
+ng = nptgroup('auto','CellName','channel0*');
+% plot all channels in array 1 (arranged vertically) using a pre-trial window of 750 ms
+InspectGUI(ng,'Object',{'vmlfp',{'PreTrial',750}},'GroupPlotSep','Vertical')
