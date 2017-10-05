a7d32c5125fcecdac963c8917eef6dfd8cce6c54
shihchengyen <30923143+shihchengyen@users.noreply.github.com>
2017-10-05 17:44:42 +0800

    Fixed bug that prevented SaveLevels and RedoLevels from working properly.

--- @rplparallel/rplparallel.m
+++ @rplparallel/rplparallel.m
@@ -49,7 +49,7 @@ elseif(strcmp(command,'createObj'))
     % IMPORTANT NOTICE!!! 
     % If there is additional requirements for creating the object, add
     % whatever needed here
-    obj = createObject(Args,varargin{:});
+    obj = createObject(Args,modvarargin{:});
 end
 
 % change back to previous directory
