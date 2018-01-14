f1503b55bf45bd3053908e4eb1f2288a0528e239
shihcheng <shihcheng@me.com>
2018-01-14 16:45:33 +0800

    Now specifies ObjectLevel as 'Session' instead of calling levelConvert to make it clearer.

--- @rplsplit/get.m
+++ @rplsplit/get.m
@@ -18,7 +18,7 @@ r = [];
 
 if(Args.ObjectLevel)
 	% specifies that the object should be created in the session directory
-	r = levelConvert('levelNo',1);
+	r = 'Session';
 elseif(Args.AnalysisLevel)
 	% specifies that the AnalysisLevel of the object is 'AllIntragroup'
 	r = 'Single';
