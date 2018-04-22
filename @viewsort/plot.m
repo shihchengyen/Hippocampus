864e9ee160b067e6701ed660545149059cdb0966
shihcheng <shihcheng@me.com>
2018-04-22 23:04:24 +0800

    New class to view waveforms returned by hmmsort.
    New class to view waveforms returned by hmmsort. The waveforms can be viewed channel by channel, array by array, session by session, or day by day.

---
+++ @viewsort/plot.m
@@ -0,0 +1,94 @@
+function [obj, varargout] = plot(obj,varargin)
+%@viewsort/plot Plot function for viewsort object.
+%   OBJ = plot(OBJ) creates a raster plot of the neuronal
+%   response.
+
+Args = struct('LabelsOff',0,'GroupPlots',1,'GroupPlotIndex',1,'Color','b', ...
+		'Array',0, 'Session',0, 'Day',0, ...
+		'ReturnVars',{''}, 'ArgsOnly',0);
+Args.flags = {'LabelsOff','ArgsOnly','Array','Session','Day'};
+[Args,varargin2] = getOptArgs(varargin,Args);
+
+% if user select 'ArgsOnly', return only Args structure for an empty object
+if Args.ArgsOnly
+    Args = rmfield (Args, 'ArgsOnly');
+    varargout{1} = {'Args',Args};
+    return;
+end
+
+if(~isempty(Args.NumericArguments))
+	% plot one data set at a time
+	n = Args.NumericArguments{1};
+	if(Args.Array || Args.Session || Args.Day)
+		if(Args.Array)
+			% plot waveforms array by array
+			% get starting row in ChannelIndex
+			chnstart = obj.data.ArrayIndex(n);
+			% get ending row in ChannelIndex
+			chnend = obj.data.ArrayIndex(n+1);
+		elseif(Args.Session)
+			% plot waveforms session by session
+			% get starting row in ChannelIndex
+			chnstart = obj.data.SessionIndex(n);
+			% get ending row in ChannelIndex
+			chnend = obj.data.SessionIndex(n+1);
+		elseif(Args.Day)
+			% plot waveforms session by session
+			% get starting row in ChannelIndex
+			chnstart = obj.data.DayIndex(n);
+			% get ending row in ChannelIndex
+			chnend = obj.data.DayIndex(n+1);
+		end  % if(Args.Array)
+		% get number of channels in this array
+		numSets = chnend - chnstart;
+	    for index = 1:numSets
+	        if(numSets>1)
+	            nptSubplot(numSets,index);
+	        end
+			chnindex = chnstart + index;
+			xind = (obj.data.ChannelIndex(chnindex)+1):obj.data.ChannelIndex(chnindex+1);
+			plot((obj.data.spikeForms(xind,:))','.-')
+			sdstr = get(obj,'SessionDirs');
+			title(getDataOrder('ShortName','DirString',sdstr{chnindex}))
+		end  % for index = 1:numSets
+	else  % if(Args.Array || Args.Session || Args.Day)
+		xind = (obj.data.ChannelIndex(n)+1):obj.data.ChannelIndex(n+1);
+		plot((obj.data.spikeForms(xind,:))','.-')
+		sdstr = get(obj,'SessionDirs');
+		title(getDataOrder('ShortName','DirString',sdstr{n}))
+	end  % if(Args.Array || Args.Session || Args.Day)
+else
+	% plot all data
+	numSets = obj.data.numSets;
+    for index = 1:numSets
+        if(numSets>1)
+            nptSubplot(numSets,index);
+        end
+		xind = (obj.data.ChannelIndex(index)+1):obj.data.ChannelIndex(index+1);
+		plot((obj.data.spikeForms(xind,:))','.-')
+		sdstr = get(obj,'SessionDirs');
+		title(getDataOrder('ShortName','DirString',sdstr{index}))
+	end
+end
+
+% add code for plot options here
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% @viewsort/PLOT takes 'LabelsOff' as an example
+if(~Args.LabelsOff)
+	xlabel('Data Points')
+	ylabel('Voltage (uV)')
+end
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+RR = eval('Args.ReturnVars');
+lRR = length(RR);
+if(lRR>0)
+    for i=1:lRR
+        RR1{i}=eval(RR{i});
+    end 
+    varargout = getReturnVal(Args.ReturnVars, RR1);
+else
+    varargout = {};
+end
---
+++ @viewsort/plus.m
@@ -0,0 +1,78 @@
+function r = plus(p,q,varargin)
+%@viewsort/plus Overloaded plus function for viewsort objects.
+%   R = plus(P,Q) combines viewsort objects P and Q and returns the
+%   viewsort object R.
+
+% get name of class
+classname = mfilename('class');
+
+% check if first input is the right kind of object
+if(~isa(p,classname))
+	% check if second input is the right kind of object
+	if(~isa(q,classname))
+		% both inputs are not the right kind of object so create empty
+		% object and return it
+		r = feval(classname);
+	else
+		% second input is the right kind of object so return that
+		r = q;
+	end
+else
+	if(~isa(q,classname))
+		% p is the right kind of object but q is not so just return p
+		r = p;
+    elseif(isempty(p))
+        % p is right object but is empty so return q, which should be
+        % right object
+        r = q;
+    elseif(isempty(q))
+        % p are q are both right objects but q is empty while p is not
+        % so return p
+        r = p;
+	else
+		% both p and q are the right kind of objects so add them 
+		% together
+		% assign p to r so that we can be sure we are returning the right
+		% object
+		r = p;
+		% useful fields for most objects
+		r.data.spikeForms = [p.data.spikeForms; q.data.spikeForms];
+		r.data.numChannels = p.data.numChannels + q.data.numChannels;
+		% object specific fields
+		r.data.ChannelIndex = [p.data.ChannelIndex; (p.data.ChannelIndex(end) ...
+			+ q.data.ChannelIndex(2:end))];
+
+		% check if p and q are from the same array
+		if(~strcmp(p.data.arrstr(end,:),q.data.arrstr))
+			r.data.arrstr = [p.data.arrstr; q.data.arrstr];
+			r.data.ArrayIndex = [p.data.ArrayIndex; (p.data.ArrayIndex(end) ...
+				+ q.data.ArrayIndex(2:end))];
+		else
+			% just update array index
+			r.data.ArrayIndex(2) = p.data.ArrayIndex(2)+ q.data.ArrayIndex(2);
+		end
+
+		% check if p and q are from the same session
+		if(~strcmp(p.data.sesstr(end,:),q.data.sesstr))
+			r.data.sesstr = [p.data.sesstr; q.data.sesstr];
+			r.data.SessionIndex = [p.data.SessionIndex; (p.data.SessionIndex(end) ...
+				+ q.data.SessionIndex(2:end))];
+		else
+			% just update array index
+			r.data.SessionIndex(2) = p.data.SessionIndex(2)+ q.data.SessionIndex(2);
+		end
+
+		% check if p and q are from the same day
+		if(~strcmp(p.data.daystr(end,:),q.data.daystr))
+			r.data.daystr = [p.data.daystr; q.data.daystr];
+			r.data.DayIndex = [p.data.DayIndex; (p.data.DayIndex(end) ...
+				+ q.data.DayIndex(2:end))];
+		else
+			% just update array index
+			r.data.DayIndex(2) = p.data.DayIndex(2)+ q.data.DayIndex(2);
+		end
+					
+		% add nptdata objects as well
+		r.nptdata = plus(p.nptdata,q.nptdata);
+	end
+end
