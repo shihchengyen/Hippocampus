556ee208261b6e8246ff58255f88694251ae753d
shihcheng <shihcheng@me.com>
2018-05-06 23:38:59 +0800

    Added option to plot the frequency spectrum.
    Added option to plot the frequency spectrum.
    
    Also modified option to plot spike trains by loading hmmsort.mat automatically.
    
    Now plots spike trains from all cells in hmmsort.mat in different colors.
    
    Added code to add a title to each plot.

--- @rplhighpass/plot.m
+++ @rplhighpass/plot.m
@@ -3,10 +3,10 @@ function [obj, varargout] = plot(obj,varargin)
 %   OBJ = plot(OBJ) creates a raster plot of the neuronal
 %   response.
 
-Args = struct('LabelsOff',0,'GroupPlots',1,'GroupPlotIndex',1,'Color','b', ...
-			'SpikeData',[], 'SpikeTriggerIndex', 26, 'SpikeHeight', 100, ...
-		    'LoadSort', 0, 'ReturnVars',{''}, 'ArgsOnly',0);
-Args.flags = {'LabelsOff','ArgsOnly','NormalizeTrial','LoadSort'};
+Args = struct('LabelsOff',0, 'GroupPlots',1, 'GroupPlotIndex',1, 'Color','b', ...
+			'SpikeData',[], 'SpikeTriggerIndex',26, 'SpikeHeight',100, ...
+		    'FFT',0, 'LoadSort',0, 'ReturnVars',{''}, 'ArgsOnly',0);
+Args.flags = {'LabelsOff','ArgsOnly','NormalizeTrial','LoadSort','FFT'};
 [Args,varargin2] = getOptArgs(varargin,Args);
 
 % if user select 'ArgsOnly', return only Args structure for an empty object
@@ -19,41 +19,57 @@ end
 if(~isempty(Args.NumericArguments))
 	% plot one data set at a time
 	n = Args.NumericArguments{1};
-	plot(obj.data.analogTime * 1000,obj.data.analogData,'.-')
-	if(Args.LoadSort)
-		l = load('hmmsort.mat');
-		Args.SpikeData = l.mlseq;
-	end
-	if(~isempty(Args.SpikeData))
-		hold on
-		% get SpikeData
-		mlseq = Args.SpikeData;
-		spidx = Args.SpikeTriggerIndex;
-		ncells = size(mlseq,1);
-		clist = nptDefaultColors(1:ncells);
-		% add spike trains
-		for spi = 1:ncells
-			st1 = find(mlseq(spi,:)==spidx);
-			% add stem plot
-			stem( obj.data.analogTime(st1) * 1000, repmat(Args.SpikeHeight,[size(st1),1]), 'Color', clist(spi,:))
+	if(Args.FFT)
+		PlotFFT(obj.data.analogData,obj.data.analogInfo.SampleRate)
+		if(~Args.LabelsOff)
+			xlabel('Freq (Hz)')
+			ylabel('Magnitude')
 		end
-		hold off
-	end	
+	else
+		plot(obj.data.analogTime * 1000,obj.data.analogData,'.-')
+		if(Args.LoadSort)
+			l = load('hmmsort.mat');
+			Args.SpikeData = l.mlseq;
+		end
+		if(~isempty(Args.SpikeData))
+			hold on
+			% get SpikeData
+			mlseq = Args.SpikeData;
+			spidx = Args.SpikeTriggerIndex;
+			ncells = size(mlseq,1);
+			clist = nptDefaultColors(1:ncells);
+			% add spike trains
+			for spi = 1:ncells
+				st1 = find(mlseq(spi,:)==spidx);
+				% add stem plot
+				stem( obj.data.analogTime(st1) * 1000, repmat(Args.SpikeHeight,[size(st1),1]), 'Color', clist(spi,:))
+			end
+			hold off
+		end
+		if(~Args.LabelsOff)
+			xlabel('Time (ms)')
+			ylabel('Voltage (uv)')
+		end
+	end
 else
 	% plot all data
-	plot(obj.data.analogTime,obj.data.analogData,'.-')
+	if(Args.FFT)
+		PlotFFT(obj.data.analogData,obj.data.analogInfo.SampleRate)
+		if(~Args.LabelsOff)
+			xlabel('Freq (Hz)')
+			ylabel('Magnitude')
+		end
+	else
+		plot(obj.data.analogTime * 1000,obj.data.analogData,'.-')
+		if(~Args.LabelsOff)
+			xlabel('Time (ms)')
+			ylabel('Voltage (uv)')
+		end
+	end
 end
 
-% add code for plot options here
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% @rplhighpass/PLOT takes 'LabelsOff' as an example
-if(~Args.LabelsOff)
-	xlabel('X Axis')
-	ylabel('Y Axis')
-end
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sdstr = get(obj,'SessionDirs');
+title(getDataOrder('ShortName','DirString',sdstr{1}))
 
 RR = eval('Args.ReturnVars');
 lRR = length(RR);
