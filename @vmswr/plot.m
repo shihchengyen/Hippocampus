44ff21242ee08e2fd27ddc93842f58445e802a79
Jee Kim <roycecacao@gmail.com>
2018-06-01 13:14:48 +0800

    working plot

--- @vmswr/plot.m
+++ @vmswr/plot.m
@@ -3,12 +3,13 @@ function [obj, varargout] = plot(obj,varargin)
 %   OBJ = plot(OBJ) creates a raster plot of the neuronal
 %   response.
 
-Args = struct('LabelsOff',0,'GroupPlots',1,'GroupPlotIndex',1,'Color','b', ...
-            'PreTrial',500,'Rms',1, ...
-            'PlotAllData',0,'RemoveLineNoise',[],'TitleOff', 0, ...
+Args = struct('LabelsOff',0,'TitleOff', 0,'Color','b', ...
+            'GroupPlots',1,'GroupPlotIndex',1,...
+            'PreTrial',500,'Rms',0,'Cmds','','DisplayTrials',0,...
+            'PlotAllData',0,'RemoveLineNoise',[],...
             'ReturnVars',{''}, 'ArgsOnly',0);
-Args.flags = {'LabelsOff','ArgsOnly', ...
-            'PlotAllData'};
+Args.flags = {'LabelsOff','TitleOff','ArgsOnly', ...
+            'PlotAllData','Rms','Cmds','DisplayTrials'};
 [Args,varargin2] = getOptArgs(varargin,Args);
 
 % if user select 'ArgsOnly', return only Args structure for an empty object
@@ -18,126 +19,187 @@ if Args.ArgsOnly
     return;
 end
 
+sRate = obj.data.analogInfo.SampleRate;
+swrInfo = obj.data.analogRmsInfo.Swr;
+
+% for title
+[a,b] = fileparts(obj.nptdata.SessionDirs{:});
+title(b)
+sp = {' '}; %space
+
 if(~isempty(Args.NumericArguments))
 	% plot one data set at a time
 	n = Args.NumericArguments{1};
-else
-	% plot all data
-	n = 1;
-    fprintf('work in progress');
-end
 
-tIdx = obj.data.trialIndices(n,:);
-sRate = obj.data.analogInfo.SampleRate;
-swrInfo = obj.data.analogRmsInfo.Swr;
-swrMean = obj.data.analogRmsInfo.Mean;
-swrStd = obj.data.analogRmsInfo.Std;
-
-if(length(tIdx)==2)
-    OldMarkerFormat = 1;
-else
-    OldMarkerFormat = 0;
-end
-
-if(OldMarkerFormat)
-    idx = (tIdx(1)-(Args.PreTrial/1000*sRate)):tIdx(2);
-else
-    idx = (tIdx(1)-(Args.PreTrial/1000*sRate)):tIdx(3);
-end
-
-if(Args.Rms)
-    idx=unique(roundn(idx./5,0));
-    if (Args.PlotAllData)
-        data = obj.data.analogRmsData';
-    else
-        data = obj.data.analogRmsData';
-        data = data(idx,:);
-    end
-    sc = 200; %scaling for graphs
-    numPlots = floor(size(data,1)/sc);
-else
-    if (Args.PlotAllData)
-        data = obj.data.analogData;
-    else
-        data = obj.data.analogData(idx,:);
-    end
-    sc = 1000;
-end
-if(~isempty(Args.RemoveLineNoise))
-        data = nptRemoveLineNoise(data,Args.RemoveLineNoise,sRate);
-end
-
-% set axis
-y0 = min(data);
-y1 = max(data);
-
-df = length(idx);
-
-% if (~isempty(gco))
-%     delete(gca)
-% end
-
-hold on
+    tIdx = obj.data.trialIndices(n,:);
+    swrMean = obj.data.analogRmsInfo.Mean;
+    swrStd = obj.data.analogRmsInfo.Std;
     
-for i = 1:length(swrInfo)
-    if swrInfo(i,3) >= idx(1) && swrInfo(i,2) <= idx(end) % detects if swr event is in the graph
-%             si = 0; %swr event
-        if swrInfo(i,3) >= idx(1) && swrInfo(i,2) < idx(1)
-            x0 = idx(1);
-            x1 = swrInfo(i,3)+0.5;
-%                 if swrInfo(i,1) >= idx(1)
-%                     si = swrInfo(i,1);
-%                 end
-        elseif swrInfo(i,2) <= idx(end) && swrInfo(i,3) > idx(end)
-            x0 = swrInfo(i,2)-0.5;
-            x1 = idx(end);
-%                 if swrInfo(i,1) <= idx(end)
-%                     si = swrInfo(i,1);
-%                 end
+    if (~Args.PlotAllData)
+        if(length(tIdx)==2)
+            OldMarkerFormat = 1;
         else
-            x0 = swrInfo(i,2)-0.5;
-            x1 = swrInfo(i,3)+0.5;
-%                 si = swrInfo(i,1);
+            OldMarkerFormat = 0;
         end
-%             si = (si-idx(1)).*(1000/sc);
-        patch(([x0 x1 x1 x0]-idx(1)).*(1000/sc), [y0*[1 1] y1*[1 1]], [0.7 0.8 1], 'LineStyle','none')
-%             if (si ~= 0)
-%                 plot(si, data(si/(1000/sc)),'rx')
-%             end
+
+        if(OldMarkerFormat)
+            idx = (tIdx(1)-(Args.PreTrial/1000*sRate)):tIdx(2);
+        else
+            idx = (tIdx(1)-(Args.PreTrial/1000*sRate)):tIdx(3);
+        end
+               
+    else
+        idx = 1:length(obj.data.analogData);
     end
+    
+    tStart = idx(1); % for 
+    tEnd = idx(end);
+    swridx = unique(roundn(idx./5,0));
+
+    if(Args.Rms)
+        sc = 1000/200; %scaling for graphs
+        idx=swridx;
+        if (Args.PlotAllData)
+            data = obj.data.analogRmsData';
+        else
+            data = obj.data.analogRmsData';
+            data = data(idx,:);
+        end
+        numPlots = floor(size(data,1)/200);
+    else
+        sc = 1000/1000;
+        if (Args.PlotAllData)
+            data = obj.data.analogData;
+        else
+            data = obj.data.analogData(idx,:);
+        end
+        
+        if(~isempty(Args.RemoveLineNoise))
+			data = nptRemoveLineNoise(data,Args.RemoveLineNoise,sRate);
+		end
+    end
+
+    if(~isempty(Args.RemoveLineNoise))
+            data = nptRemoveLineNoise(data,Args.RemoveLineNoise,sRate);
+    end
+
+    % set axis
+    y0 = min(data);
+    y1 = max(data);
+
+    di = idx(1);
+    df = idx(end);
+
+    % if (~isempty(gco))
+    %     delete(gca)
+    % end
+
+    hold on
+
+    for i = 1:length(swrInfo)
+        if swrInfo(i,3) >= swridx(1) && swrInfo(i,2) <= swridx(end) % detects if swr event is in the graph
+    %             si = 0; %swr event
+            if swrInfo(i,3) >= swridx(1) && swrInfo(i,2) < swridx(1)
+                x0 = swridx(1);
+                x1 = swrInfo(i,3)+0.5;
+    %                 if swrInfo(i,1) >= swridx(1)
+    %                     si = swrInfo(i,1);
+    %                 end
+            elseif swrInfo(i,2) <= swridx(end) && swrInfo(i,3) > swridx(end)
+                x0 = swrInfo(i,2)-0.5;
+                x1 = swridx(end);
+    %                 if swrInfo(i,1) <= swridx(end)
+    %                     si = swrInfo(i,1);
+    %                 end
+            else
+                x0 = swrInfo(i,2)-0.5;
+                x1 = swrInfo(i,3)+0.5;
+    %                 si = swrInfo(i,1);
+            end
+    %             si = (si-swridx(1)).*sc;
+            patch(([x0 x1 x1 x0]).*5, [y0*[1 1] y1*[1 1]], [1 0.1 0.1], 'LineStyle','none')
+    %             if (si ~= 0)
+    %                 plot(si, data(si/sc),'rx')
+    %             end
+        end
+    end
+
+    plot((di:df)*sc, data,'Color',Args.Color);
+
+    ylim([y0 y1])
+    xlim([di*sc df*sc])
+
+    % plotting std away from mean
+    plot(xlim,[swrMean+3*swrStd swrMean+3*swrStd],'g');
+    plot(xlim,[swrMean+swrStd swrMean+swrStd],'g'); 
+    text(di*sc, swrMean+3*swrStd, 'threshold = 3\sigma');
+    text(di*sc, swrMean+swrStd, '1\sigma');
+    % p = gco;
+    hold off
+    
+    % texts for title, yaxis and xaxis 
+    tt = strcat('LFP of',sp,b,sp,'from',sp,num2str(tStart),'ms to',sp,num2str(tEnd),'ms');
+    ty = 'LFP';
+    tx = 'time (ms)';
+else
+  
+    tIdx = obj.data.trialIndices;
+    s = size(tIdx);
+    tIdx(:,1) = (tIdx(:,1)-(Args.PreTrial/1000*sRate))./5;
+    
+    if(s(2)==2)
+        OldMarkerFormat = 1;
+    else
+        OldMarkerFormat = 0;
+    end
+    
+    if(OldMarkerFormat)
+        tIdx(:,2) = (tIdx(:,2))./5;
+    else
+        tIdx(:,2) = (tIdx(:,3))./5;
+    end
+    
+    % count SPW Event in each trial 
+    for i = 1:length(tIdx)
+        tIdx(i,3) = sum(swrInfo(:,1)>tIdx(i,1) & swrInfo(:,1)<tIdx(i,2));
+    end
+    
+    histogram(tIdx(:,3))
+    
+    % texts for title, yaxis and xaxis 
+    tt = strcat('Histogram of SPW Events in',sp,b,sp,'in each trial');
+    ty = 'Trial';
+    tx = 'SPW Event (count)';
 end
-
-plot((1:df)*(1000/sc), data,'Color',Args.Color);
-
-% plotting std away from mean
-plot(xlim,[swrMean+3*swrStd swrMean+3*swrStd],'r');
-plot(xlim,[swrMean+swrStd swrMean+swrStd],'r'); 
-text(1*(1000/sc)+5, 1+swrMean+3*swrStd, 'threshold = 3\sigma');
-text(1*(1000/sc)+5, 1+swrMean+swrStd, '1\sigma');
-p = gco;
-ylim([y0 y1])
-
-hold off
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% need to move this in due to key input
-% @vmswr/PLOT takes 'LabelsOff'
+% @vmswr/PLOT takes 'LabelsOff' and 'TitleOff'
 Args.LabelsOff = 0;
 
 if(~Args.LabelsOff)
-    xlabel('time (ms)')
-    ylabel('LFP')
-    Args.LabelsOff = 1;
+    xlabel(tx)
+    ylabel(ty)
 end
 
 if(~Args.TitleOff)
-    [a,b] = fileparts(obj.nptdata.SessionDirs{:});
-    title(b)
-    sp = {' '}; %space
-    title(strcat('LFP of',sp,b,sp,'from',sp,num2str(idx(1)),'ms to',sp,num2str(idx(end)),'ms'))
+    title(tt)
 end
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if(~isempty(Args.Cmds))
+    % save the current figure in case Args.Cmds switches to another figure
+    h = gcf;
+    eval(Args.Cmds)
+    % switch back to previous figure
+    figure(h);
+end
+
+if(Args.DisplayTrials)
+    fprintf('here')
+    vr = vmswr('auto');
+    InspectGUI(vr,'addObjs',{vr},'optArgs',{{'PlotAllData'},{'Rms'}},'SP',[2 1])
+end
 
 % hidata plot overwrites the x and y label
 RR = eval('Args.ReturnVars');
