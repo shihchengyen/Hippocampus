b4e828ec96a05adeef99e491202b4a242a86339d
Jee Kim <roycecacao@gmail.com>
2018-06-05 15:11:02 +0800

    bandselect (bandpower)
    adapted from placeselect

---
+++ @viewband/plot.m
@@ -0,0 +1,111 @@
+function [obj, varargout] = plot(obj,varargin)
+%@viewband/plot Plot function for viewband object.
+%   OBJ = plot(OBJ) creates a raster plot of the neuronal
+%   response.
+
+Args = struct('LabelsOff',0,'GroupPlots',1,'GroupPlotIndex',1,'Color','b', ...
+		'Channel',0, 'Array',0, 'Session',0, 'Day',0, ...
+		'ReturnVars',{''}, 'ArgsOnly',0);
+Args.flags = {'LabelsOff','ArgsOnly','Channel','Array','Session','Day'};
+[Args,varargin2] = getOptArgs(varargin,Args);
+
+% if user select 'ArgsOnly', return only Args structure for an empty object
+if Args.ArgsOnly
+    Args = rmfield (Args, 'ArgsOnly');
+    varargout{1} = {'Args',Args};
+    return;
+end
+
+if(~isempty(Args.NumericArguments))
+	% plot one data set at a time
+	n = Args.NumericArguments{1};
+	if(Args.Channel || Args.Array || Args.Session || Args.Day)
+		if(Args.Channel)
+			% plot waveforms array by array
+			% get starting row in ChannelIndex
+			sistart = obj.data.ChannelIndex(n)+1;
+			% get ending row in ChannelIndex
+			siend = obj.data.ChannelIndex(n+1);			
+			stem(obj.data.locSI(sistart:siend))
+			sdstr = get(obj,'SessionDirs');
+			title(getDataOrder('ShortName','DirString',sdstr{n}))
+		elseif(Args.Array)
+			% plot waveforms array by array
+			% get starting row in ChannelIndex
+			sistart = obj.data.ArrayIndex(n)+1;
+			% get ending row in ChannelIndex
+			siend = obj.data.ArrayIndex(n+1);
+			stem(obj.data.locSI(sistart:siend))
+			title(obj.data.arrstr(n,:))
+		elseif(Args.Session)
+			% plot waveforms session by session
+			% get starting row in ChannelIndex
+			sistart = obj.data.SessionIndex(n)+1;
+			% get ending row in ChannelIndex
+			siend = obj.data.SessionIndex(n+1);
+			stem(obj.data.locSI(sistart:siend))
+			title(obj.data.sesstr(n,:))
+		elseif(Args.Day)
+			% plot waveforms session by session
+			% get starting row in ChannelIndex
+			sistart = obj.data.DayIndex(n)+1;
+			% get ending row in ChannelIndex
+			siend = obj.data.DayIndex(n+1);
+			stem(obj.data.locSI(sistart:siend))
+			title(obj.data.daystr(n,:))
+		end  % if(Args.Channel)
+		if(~Args.LabelsOff)
+			xlabel('Cells')
+			ylabel('Place Selectivity Index')
+		end
+	else  % if(Args.Channel || Args.Array || Args.Session || Args.Day)
+		% display png file for each unit
+		% first get the directory
+		sidx = find(obj.data.ChannelIndex>=n);
+		sdstr = get(obj,'SessionDirs');
+		cwd = pwd;
+		sidx1 = sidx(1) - 1;
+% 		cd(sdstr{sidx1})
+        path = strrep(sdstr{sidx1},'/Users/shihcheng/afp','/');
+        cd(path)
+		% need to figure which picture inside this directory we need to load
+		pnum = n - obj.data.ChannelIndex(sidx1);
+% 		% load the picture
+% 		a = imread('bandfield.png');
+% 		imshow(a)
+        g = gcf;
+        axis off
+        delete(findall(findall(gcf,'Type','axe'),'Type','text')) %clear texts
+        a = openfig('bandfield.fig','invisible');
+        copyobj(allchild(a),g);
+        close(a)
+        
+%         a = open('bandfield.fig');
+%         get gcf
+%         plot(a)
+		cd(cwd)
+	end  % if(Args.Array || Args.Session || Args.Day)
+else
+	% plot all data
+	stem(obj.data.locSI)
+	% ci = obj.data.ChannelIndex(2:end);
+	% nci = size(ci,1);
+	% cix = repmat((ci+0.5)',2,1);
+	% ciy = repmat(ylim',1,nci);
+	% line(cix,ciy)
+	if(~Args.LabelsOff)
+		xlabel('Cells')
+		ylabel('Place Selectivity Index')
+	end
+end
+
+RR = eval('Args.ReturnVars');
+lRR = length(RR);
+if(lRR>0)
+    for i=1:lRR
+        RR1{i}=eval(RR{i});
+    end 
+    varargout = getReturnVal(Args.ReturnVars, RR1);
+else
+    varargout = {};
+end
---
+++ @viewband/viewband.m
@@ -0,0 +1,114 @@
+function [obj, varargout] = viewband(varargin)
+%@viewband Constructor function for viewband class
+%   OBJ = viewband(varargin)
+%
+%   OBJ = viewband('auto') attempts to create a viewband object by ...
+%   
+%   %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   % Instructions on viewband %
+%   %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%example [as, Args] = viewband('save','redo')
+%
+%dependencies: 
+
+Args = struct('RedoLevels',0, 'SaveLevels',0, 'Auto',0, 'ArgsOnly',0, ...
+				'FileName','bandfield.mat', 'ObjectLevel','Channel');
+Args.flags = {'Auto','ArgsOnly'};
+% Specify which arguments should be checked when comparing saved objects
+% to objects that are being asked for. Only arguments that affect the data
+% saved in objects should be listed here.
+Args.DataCheckArgs = {};                            
+
+[Args,modvarargin] = getOptArgs(varargin,Args, ...
+	'subtract',{'RedoLevels','SaveLevels'}, ...
+	'shortcuts',{'redo',{'RedoLevels',1}; 'save',{'SaveLevels',1}}, ...
+	'remove',{'Auto'});
+
+% variable specific to this class. Store in Args so they can be easily
+% passed to createObject and createEmptyObject
+Args.classname = 'viewband';
+Args.matname = [Args.classname '.mat'];
+Args.matvarname = 'vb';
+
+% To decide the method to create or load the object
+[command, robj] = checkObjCreate('ArgsC',Args,'narginC',nargin,'firstVarargin',varargin);
+
+if(strcmp(command,'createEmptyObjArgs'))
+    varargout{1} = {'Args',Args};
+    obj = createEmptyObject(Args);
+elseif(strcmp(command,'createEmptyObj'))
+    obj = createEmptyObject(Args);
+elseif(strcmp(command,'passedObj'))
+    obj = varargin{1};
+elseif(strcmp(command,'loadObj'))
+	obj = robj;
+elseif(strcmp(command,'createObj'))
+    % IMPORTANT NOTICE!!! 
+    % If there is additional requirements for creating the object, add
+    % whatever needed here
+    obj = createObject(Args,modvarargin{:});
+end
+
+function obj = createObject(Args,varargin)
+
+% example object
+dlist = dir(Args.FileName);
+% get entries in directory
+dnum = size(dlist,1);
+
+% check if the right conditions were met to create object
+if(dnum>0)
+	% these are fields that are useful for most objects
+	data.numChannels = 1;
+
+	data.locSI = zeros(dnum,1);
+	for didx = 1:dnum
+		l = load(dlist(didx).name);
+        data.thetaTime(didx,:,:) = l.theta_time;
+        data.thetaPowerSpec(didx,:,:) = l.tPowerSpec;
+        data.alphaTime(didx,:,:) = l.alpha_time;
+        data.alphaPowerSpec(didx,:,:) = l.aPowerSpec;
+        data.gammaTime(didx,:,:) = l.gamma_time;
+        data.gammaPowerSpec(didx,:,:) = l.gPowerSpec;
+	end
+	% set index to keep track of which data goes with which directory
+	data.ChannelIndex = [0; dnum];
+	data.ArrayIndex = [0; dnum];
+	data.SessionIndex = [0; dnum];
+	data.DayIndex = [0; dnum];
+	% get channel string
+	[data.arrstr(1,:), chnstr] = nptFileParts(pwd);
+	% get array string
+	[data.sesstr(1,:), arrstr] = nptFileParts(data.arrstr(1,:));
+	% get session string
+	[data.daystr(1,:), sesstr] = nptFileParts(data.sesstr(1,:));
+	% get day string
+	% [p4, data.daystr(1,:)] = nptFileParts(p3);
+    data.Args = Args;
+		
+	% create nptdata so we can inherit from it    
+    data.Args = Args;
+	n = nptdata(data.numChannels,0,pwd);
+	d.data = data;
+	obj = class(d,Args.classname,n);
+	saveObject(obj,'ArgsC',Args);
+else
+	% create empty object
+	obj = createEmptyObject(Args);
+end
+
+function obj = createEmptyObject(Args)
+
+% useful fields for most objects
+data.numChannels = 0;
+data.numArrays = 0;
+data.numSessions = 0;
+data.numDays = 0;
+data.locSI = [];
+
+% create nptdata so we can inherit from it
+data.Args = Args;
+n = nptdata(0,0);
+d.data = data;
+obj = class(d,Args.classname,n);
