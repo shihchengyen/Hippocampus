86e0482013635cecccaa114c8eccf47e18b8ce30
akihumii <a0126587@u.nus.edu>
2018-10-05 14:34:59 +0800

    fixed the bug of missing the eye and test plots, remove getting the screensize as the image size, increase the resolution of the figure, ignore the channel above 124

--- plotStackedFFT.m
+++ plotStackedFFT.m
@@ -3,7 +3,7 @@ function [] = plotStackedFFT()
 %the current directory, plot the FFT png togther in dirrent figures showing
 %each array, referred to certain geometry specified in function
 %'assignChannelGeo'
-% 
+%
 %   Detailed explanation goes here
 
 numRow = 5;
@@ -14,12 +14,13 @@ channelGeo = assignChannelGeo(numRow,numArray); % a function to get the channel 
 hpInfo = dir('**/hp.png');
 
 lfpInfo = dir('**/lfp.png');
-numLfp = length(lfpInfo);
 
 %% plot stacked hp.png
 close all
 
 numCol = size(channelGeo,2);
+
+resolutionValue = '-r1200'; % -r###, ### is the resolution value
 
 offsetX = 1/numCol;
 offsetY = 1/numRow;
@@ -27,29 +28,36 @@ setX = 0 : offsetX : 1;
 setY = fliplr(0 : offsetY : 1-offsetY);
 
 [pHp,pEHp,pTHp] = insertFFT(hpInfo,numArray,channelGeo,setX,setY,offsetX,offsetY);
+
 for i = 1:numArray
-    saveas(pHp(i,1),['session01',filesep,'array0',num2str(i),filesep,'stackedhp01.png']);
+    set(pHp(i,1), 'Visible', 'on');
+    print(['session01',filesep,'array0',num2str(i),filesep,'stackedhp01'],'-dpng',resolutionValue);
     delete(pHp(i,1));
     
-    saveas(pEHp(i,1),['sessioneye',filesep,'array0',num2str(i),filesep,'stackedhpeye.png']);
+    set(pEHp(i,1), 'Visible', 'on');
+    print(['sessioneye',filesep,'array0',num2str(i),filesep,'stackedhpeye'],'-dpng',resolutionValue);
     delete(pEHp(i,1));
-
-    saveas(pTHp(i,1),['sessiontest',filesep,'array0',num2str(i),filesep,'stackedhptest.png']);
+    
+    set(pTHp(i,1), 'Visible', 'on');
+    print(['sessiontest',filesep,'array0',num2str(i),filesep,'stackedhptest.png'],'-dpng',resolutionValue);
     delete(pTHp(i,1));
-end    
+end
 
 
-[pLfp,pELfp,PTLfp] = insertFFT(lfpInfo,numArray,channelGeo,setX,setY,offsetX,offsetY);
+[pLfp,pELfp,pTLfp] = insertFFT(lfpInfo,numArray,channelGeo,setX,setY,offsetX,offsetY);
 for i = 1:numArray
-    saveas(pLfp(i,1),['session01',filesep,'array0',num2str(i),filesep,'stackedlfp01.png']);
+    set(pLfp(i,1), 'Visible', 'on');
+    print(pLfp(i,1),['session01',filesep,'array0',num2str(i),filesep,'stackedlfp01.png'],'-dpng',resolutionValue);
     delete(pLfp(i,1));
     
-    saveas(pELfp(i,1),['sessioneye',filesep,'array0',num2str(i),filesep,'stackedlfpeye.png']);
+    set(pELfp(i,1), 'Visible', 'on');
+    print(pELfp(i,1),['sessioneye',filesep,'array0',num2str(i),filesep,'stackedlfpeye.png'],'-dpng',resolutionValue);
     delete(pELfp(i,1));
-
-    saveas(pTLfp(i,1),['sessiontest',filesep,'array0',num2str(i),filesep,'stackedlfptest.png']);
+    
+    set(pTLfp(i,1), 'Visible', 'on');
+    print(pTLfp(i,1),['sessiontest',filesep,'array0',num2str(i),filesep,'stackedlfptest.png'],'-dpng',resolutionValue);
     delete(pTLfp(i,1));
-end    
+end
 
 disp('Done...')
 
@@ -71,53 +79,53 @@ end
 
 channelGeo(:,:,4) = vertcat( ... % last array
     fliplr(reshape(transpose(reshape(channelTbl(4,1):channelTbl(4,2),[],numRow-2)),numRow-2,[])),...
-    horzcat(fliplr(119:125), nan),...
-    horzcat(fliplr(126:132), nan));
-    %nan(1,size(channelGeo,2)));
+    horzcat(nan, fliplr(119:124), nan),...
+    nan(1,size(channelGeo,2)));
 
 
 end
 
 function [p,pE,pT] = insertFFT(info,numArray,channelGeo,setX,setY,offsetX,offsetY)
 %INSERTFFT Insert the png into its corresponding figure
-%   Detailed explanation goes here
+%   [p,pE,pT] = insertFFT(info,numArray,channelGeo,setX,setY,offsetX,offsetY)
+
+p = gobjects(numArray,1);
+pE = gobjects(numArray,1);
+pT = gobjects(numArray,1);
+
+set(0, 'DefaultFigureVisible', 'off');
 
 for i = 1:numArray
     p(i,1) = figure; % for session01
-    set(gcf, 'Position', get(0,'Screensize')-[0 0 0 80],'PaperPositionMode', 'auto');
     pE(i,1) = figure; % for sessioneye
-    set(gcf, 'Position', get(0,'Screensize')-[0 0 0 80],'PaperPositionMode', 'auto');
     pT(i,1) = figure; % for sessioneye
-    set(gcf, 'Position', get(0,'Screensize')-[0 0 0 80],'PaperPositionMode', 'auto');
-
 end
 
 numP = length(info);
 
 for i = 1:numP
     indexArray = strfind(info(i).folder,'array');
-    arrayNum = str2num(info(i).folder(indexArray+5:indexArray+6)); % array number
+    arrayNum = str2double(info(i).folder(indexArray+5:indexArray+6)); % array number
     
     indexChannel = strfind(info(i).folder,'channel');
-    channelNum = str2num(info(i).folder(indexChannel+7:indexChannel+9)); % channel number
+    channelNum = str2double(info(i).folder(indexChannel+7:indexChannel+9)); % channel number
     
-    [y,x] = find(channelGeo(:,:,arrayNum) == channelNum); % location of the channel in channel geometry    
-    
-    pTemp = fullfile(info(i,1).folder,info(i,1).name);
-
-    if ~isempty(strfind(info(i,1).folder,'01'))
-        set(0,'CurrentFigure',p(arrayNum,1)); % change to the target figure of session01
-    elseif ~isempty(strfind(info(i,1).folder,'eye'))
-        set(0,'CurrentFigure',pE(arrayNum,1)); % change to the target figure of sessiontest
-    elseif ~isempty(strfind(info(i,1).folder,'test'))
-        set(0,'CurrentFigure',pT(arrayNum,1)); % change to the target figure of sessiontest
-    else
-      warning('png appeared in inproper directory...') 
+    if channelNum < 125
+        [y,x] = find(channelGeo(:,:,arrayNum) == channelNum); % location of the channel in channel geometry
+        
+        pTemp = fullfile(info(i,1).folder,info(i,1).name);
+        
+        if ~isempty(strfind(info(i,1).folder,'eye'))
+            set(0,'CurrentFigure',pE(arrayNum,1)); % change to the target figure of sessiontest
+        elseif ~isempty(strfind(info(i,1).folder,'test'))
+            set(0,'CurrentFigure',pT(arrayNum,1)); % change to the target figure of sessiontest
+        else
+            set(0,'CurrentFigure',p(arrayNum,1)); % change to the target figure of session01
+        end
+        
+        axes('pos',[setX(x), setY(y), offsetX, offsetY]);
+        imshow(pTemp)
     end
-    
-    axes('pos',[setX(x), setY(y), offsetX, offsetY]);
-    imshow(pTemp)
-    
 end
 
 end
