d4e03aae74dc7e1f346d1a364773fd1fa9128b6d
shihcheng <shihcheng@me.com>
2019-04-19 11:31:58 +0800

    Added code to compute the number of trials in which the animal took the shortest path to the destination for each session.
    Added code to compute the number of trials in which the animal took the shortest path to the destination for each session.

--- @unitymaze/get.m
+++ @unitymaze/get.m
@@ -10,8 +10,9 @@ function [r,varargout] = get(obj,varargin)
 %   Dependencies: 
 
 Args = struct('ObjectLevel',0, 'AnalysisLevel',0, 'GroupPlotProperties',0, ...
-				'Number',0, 'Trial',0);
-Args.flags ={'ObjectLevel','AnalysisLevel','GroupPlotProperties','Number','Trial'};
+				'Number',0, 'Trial',0,'NumProcessTrials',0,'ProcessTrialsDiff',-100);
+Args.flags ={'ObjectLevel','AnalysisLevel','GroupPlotProperties','Number', ...
+    'Trial','NumProcessTrials'};
 Args = getOptArgs(varargin,Args);
 
 % set variables to default
@@ -27,6 +28,20 @@ elseif(Args.GroupPlotProperties)
 	r = 'Vertical';
 elseif(Args.Number && Args.Trial)
 	r = obj.data.setIndex(end);
+elseif(Args.NumProcessTrials)
+    % get processTrial field
+    pT = obj.data.processTrials;
+    % take the difference between consecutive points to find the transition
+    % between sessions
+    di = diff(pT);
+    % find the indices where the difference is more negative than -100
+    dim = find(di<Args.ProcessTrialsDiff);
+    % find the difference between those indices to get the number of trials
+    % per session
+    dimd = diff(dim);
+    % this will be missing the number of trials from the first and last 
+    % sessions so we will need to add and compute those separately
+    r = [dim(1); dimd; size(pT,1)-dim(end)];
 else
 	% if we don't recognize and of the options, pass the call to parent
 	% in case it is to get number of events, which has to go all the way
