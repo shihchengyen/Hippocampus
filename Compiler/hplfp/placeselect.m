94faea0c4edc2d4e14b6b23afa49c9564f43208c
Shih-Cheng Yen <syen@YSC-Mac.local>
2019-05-12 16:00:21 +0800

    Added option to use all trials, instead of just the 1-shot trials, to compute mean firing rates.
    Added option to use all trials, instead of just the 1-shot trials, to compute mean firing rates.
    
    Fixed bug that resulted in the spiketimes being stored with the wrong units.
    
    Added code to compute SIC, as well as the circularly shifted spike trains for the shuffle analysis.

--- Compiler/hplfp/placeselect.m
+++ Compiler/hplfp/placeselect.m
@@ -22,7 +22,11 @@ gridPosition = um.data.gridPosition;
 gpDurations = um.data.gpDurations;
 unityTrialTime = um.data.unityTrialTime;
 ntrials = size(unityTriggers,1);
-nptrials = size(processTrials,1);
+if(Args.UseAllTrials)
+	nptrials = ntrials;
+else
+	nptrials = size(processTrials,1);
+end
 
 sampleRate = rp.data.SampleRate;
 % compute trial durations from the timestamps in rplparallel
@@ -33,7 +37,9 @@ trialcount = 0; % count total number of trials
 spikecountSess = 0; % count number of spikes (per trial)
 
 % spiketrain = load('spiketrain.mat');
-sTimes = spiketrain.timestamps;
+% convert spike times to seconds to make it consistent with Ripple
+% markers and Unity timestamps
+sTimes = spiketrain.timestamps/1000;
 
 % create memory
 spikeLocTrial = zeros(gridBins,nptrials);
@@ -42,7 +48,12 @@ tDiff = nan(nptrials,1);
 
 % loop over number of trials            
 for npi = 1:nptrials
-    g = processTrials(npi);
+	if(Args.UseAllTrials)
+		g = npi;
+	else
+	    g = processTrials(npi);
+	end
+    fprintf(1,'Processing Trial %i\n',g);
     
 	tstart = rp.data.timeStamps(g,2);
 	tend = rp.data.timeStamps(g,3);
@@ -54,6 +65,7 @@ for npi = 1:nptrials
 	tgp = gridPosition(uDidx);
 
 	temp = find(sTimes > tstart & sTimes < tend);
+    fprintf(1,'Found %i spikes between %f and %f\n',size(temp,2),tstart,tend);
 	% get spike times aligned to cue offset time
 	trialSpkTimes = sTimes(temp) - tstart; 
 	valIndices = ~isnan(unityTrialTime(:,g));
@@ -72,6 +84,7 @@ for npi = 1:nptrials
 		% we then call histcounts again to count number of times each grid position appears
 		% to get spikeLocTrial to be 0     0     2     0     0     0     0     0     0     0     0     0     0     0     0     0     0     5     0     0     0     0     1     3
 		% need to remove 0’s from bins in case there were any spikes that were just outside the edges of the histcounts
+		% compute histogram in parallel, and then extract the 1st column for data and the rest for shuffle
 		spikeLocTrial(:,npi) = histcounts(tgp(bins(bins~=0)),um.data.gpEdges);
 	end
 end 
@@ -80,13 +93,100 @@ end
 % set them to 0 to avoid having outliers caused by a single observation
 spikeLocTrial( (sum(spikeLocTrial>0,2)<Args.MinTrials) ,:) = 0;
 % we divide by gpDurations to get number of spikes per duration
-anovaMatrix = spikeLocTrial./gpDurations(:,processTrials);
+if(Args.UseAllTrials)
+	anovaMatrix = spikeLocTrial./gpDurations;
+else
+	anovaMatrix = spikeLocTrial./gpDurations(:,processTrials);
+end
 meanFRs = nanmean(anovaMatrix,2);
 semFRs = nanstd(anovaMatrix,0,2)./sqrt(sum(~isnan(anovaMatrix),2));
 
+% compute SIC
+% compute total duration for each position
+if(Args.UseAllTrials)
+	o_i = sum(gpDurations,2);
+else
+	o_i = sum(gpDurations(:,processTrials),2);
+end
+% compute total duration across all positions
+So_i = sum(o_i);
+% compute proportion of occupied time
+P_i = o_i/So_i;
+% compute mean firing rate over all positions weighed by proportion of
+% occupied time
+% replace NaN with 0 in meanFRs;
+lambda_i = meanFRs;
+lambda_i(isnan(lambda_i)) = 0;
+lambda_bar = P_i' * lambda_i;
+% divide firing for each position by the overall mean
+FRratio = lambda_i/lambda_bar;
+% compute first term in SIC
+SIC1 = P_i .* FRratio;
+SIC2 = log2(FRratio);
+SIC2(isinf(SIC2)) = 0;
+SIC = SIC1' * SIC2;
+
+% compare SIC to SIC from shuffled spike trains
+% remove spikes that occurred after the unity program ended to avoid
+% problems with the histogram function
+% get max time, i.e. last marker in the session
+maxTime = um.data.unityTime(end);
+si = find(sTimes>maxTime);
+% get number of spikes
+if(isempty(si))
+    % no spikes greater than unityTime so we will just use all the spikes
+    nSpikes = size(sTimes,2);
+else
+    % ignore spikes after unityTime
+    nSpikes = si(1) - 1;
+end
+% create shuffled spikes array
+shuffleSize = Args.NumShuffles + 1;
+spTimes = repmat(sTimes(1:nSpikes)',1,shuffleSize);
+
+% generate circularly shifted spike trains
+% first seed the random number generator to make sure we get a different
+% sequence of numbers
+rng('shuffle');
+% generate 1000 random time shifts between 0.1 and 0.9 of maxTime
+tShifts = ((rand([1,Args.NumShuffles])*diff(Args.ShuffleLimits))+Args.ShuffleLimits(1))*maxTime;
+shuffleInd = 2:shuffleSize;
+spTimes(:,shuffleInd) = sort(mod(spTimes(:,shuffleInd) + tShifts,maxTime));
+% compute histogram of data and shuffled data
+[n,bin] = histcie(spTimes,um.data.unityTime);
+% compute histogram for location
+[n2,bin2] = histcie(gridPosition(bin),um.data.gpEdges);
+% compute SIC
+% compute total duration for each position
+o_i = sum(gpDurations,2);
+% compute total duration across all positions
+So_i = sum(o_i);
+% compute proportion of occupied time
+P_i = o_i/So_i;
+% compute mean firing rate over all positions weighed by proportion of
+% occupied time
+% replace NaN with 0 in meanFRs;
+lambda_i = n2(1:gridBins,:) ./ repmat(o_i,1,shuffleSize);
+lambda_i(isnan(lambda_i)) = 0;
+lambda_bar = P_i' * lambda_i;
+% divide firing for each position by the overall mean
+FRratio = lambda_i ./ repmat(lambda_bar,gridBins,1);
+% compute first term in SIC
+SIC1 = repmat(P_i,1,shuffleSize) .* FRratio;
+SIC2 = log2(FRratio);
+SIC2(isinf(SIC2)) = 0;
+SICsh = sum(SIC1 .* SIC2);
+
 data.gridSteps = um.data.gridSteps;
-data.meanFRs = meanFRs;
-data.semFRs = semFRs;
+if(Args.FRSIC)
+	data.meanFRs = lambda_i;
+	data.semFRs = [];
+else
+	data.meanFRs = meanFRs;
+	data.semFRs = semFRs;
+end
+data.SIC = SIC;
+data.SICsh = SICsh';
 
 if(isdeployed)
 	% save data into a mat file
