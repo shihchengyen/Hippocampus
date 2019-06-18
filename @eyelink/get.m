a882b6c8f49ea597378a9e9ea1ba95aa996a03dc
Deeksha Tewari <deeksha.tewari@mail.utoronto.ca>
2019-06-18 11:03:20 +0800

    Modified @eyelink object constructor to account for missing events in the edf file, and wrote a script that fills in the missing times using ripple data. Added an argument in the constructor that allows the user to make an object for the calibration sessions. The plotting function plots the x and y positions from this object.

--- @eyelink/get.m
+++ @eyelink/get.m
@@ -9,8 +9,8 @@ function [r,varargout] = get(obj,varargin)
 %
 %   Dependencies: 
 
-Args = struct('ObjectLevel',0, 'AnalysisLevel',0, 'Number', 0, 'Trial', 0);
-Args.flags ={'ObjectLevel','AnalysisLevel', 'Number', 'Trial'};
+Args = struct('ObjectLevel',0, 'AnalysisLevel',0, 'Number', 0, 'Trial', 0, 'XY', 0, 'Calibration', 0);
+Args.flags ={'ObjectLevel','AnalysisLevel', 'Number', 'Trial', 'XY', 'Calibration'};
 Args = getOptArgs(varargin,Args);
 
 % set variables to default
@@ -22,14 +22,15 @@ if(Args.ObjectLevel)
 elseif(Args.AnalysisLevel)
 	% specifies that the AnalysisLevel of the object is 'AllIntragroup'
 	r = 'Single';
-elseif (Args.Number && Args.Trial)
+elseif (Args.Number && (Args.Trial || Args.XY))
     %get the number of trials that are in the object
     %add every third column of the matrix 
     
    n = obj.data.noOfTrials;
    disp (n);
    r = sum (n,2);
-    
+elseif (Args.Number && Args.Calibration) 
+    n = size(obj.data.trial_timestamps,1); 
 else 
 	% if we don't recognize and of the options, pass the call to parent
 	% in case it is to get number of events, which has to go all the way
