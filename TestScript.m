8c50cdb222a230b580d8ec969c3b5d6820bed1cb
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2019-07-24 23:50:16 +0800

    Script containing some commands that can be used to test to make sure any new changes are not going to break existing functionality.

---
+++ TestScript.m
@@ -0,0 +1,12 @@
+ProcessLevel(nptdata,'Levels','Days','Include',{'20181102'},'nptLevelCmd',{'Channel','vmlfp(''auto'',''redo'')'});
+ProcessLevel(nptdata,'Levels','Days','Include',{'20181102'},'nptLevelCmd',{'Channel','vmhighpass(''auto'',''redo'')'});
+ProcessLevel(nptdata,'Levels','Days','Include',{'20181102'},'nptLevelCmd',{'Session','unitymaze(''auto'',''redo'')'});
+vc = ProcessLevel(vmplacecell,'Levels','Days','Include',{'20181102'});
+ProcessLevel(nptdata,'Levels','Days','Include',{'20181102'},'nptLevelCmd',{'Session','eyelink(''auto'',''redo'')'});
+cd 20181102/session01
+um = unitymaze('auto','GridSteps',40);
+el = eyelink('auto');
+InsepctGUI(um,'addObjs',{el},'optArgs',{{'Trial'},{'Trial'}},'SP',[2 1])
+cd ../..
+% unityfile
+% raycast
