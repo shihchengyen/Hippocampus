62d07b4fcacca0bfcec0318382881f3de293eb8c
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2019-07-27 23:13:38 +0800

    Revert "Removed ObjectLevel argument, which is specified in the get function."
    This reverts commit a662d7b147c9daed420e70be6acfed3616848f5a.

--- @rplparallel/rplparallel.m
+++ @rplparallel/rplparallel.m
@@ -13,7 +13,7 @@ function [obj, varargout] = rplparallel(varargin)
 %dependencies: 
 
 Args = struct('RedoLevels',0, 'SaveLevels',0, 'Auto',0, 'ArgsOnly',0, ...
-				'Data',[]);
+				'Data',[], 'ObjectLevel','Session');
 Args.flags = {'Auto','ArgsOnly'};
 % The arguments which can be neglected during arguments checking
 Args.DataCheckArgs = {};                            
