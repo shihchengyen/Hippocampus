d3acfca80100235e0a787736579afc17df1ed12e
yuc125 <71300761+yuc125@users.noreply.github.com>
2021-05-28 09:52:39 -0400

    Update test_ise.m

--- test_ise.m
+++ test_ise.m
@@ -5,6 +5,8 @@ function [obj, varargout] = test_ise(savefig,varargin)
 % Batch 
     % Load cell list
     cwd = '/Users/yuhsuan/Hippocampus/Data/picasso-misc/AnalysisHM/Current Analysis';
+%old: cwd = '/Volumes/Hippocampus/Data/picasso-misc/AnalysisHM/Current
+%     Analysis'
     fid = fopen([cwd '/cell_list.txt'],'rt');
 %     fid = fopen([cwd '/cell_list_1pxFiltAll.txt'],'rt');
     cellList = textscan(fid,'%s','Delimiter','\n');
@@ -14,7 +16,7 @@ function [obj, varargout] = test_ise(savefig,varargin)
 for ss = 1:size(cellList,1)
 
 % % % %     generate new vmsv
-    cd(cell2str(cellList(ss)));
+    cd(strrep(cell2str(cellList(ss))),'Volumes','Users/yuhsuan');%directory specific to my workstation
     sv=vmsv('auto');
 %     get each cell's ise
     ise=[ise(:) sv.data.ISE];
