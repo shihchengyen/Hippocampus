22437b5b094da30abbc4e98f9d2fcd55a7e8b26e
yuc125 <71300761+yuc125@users.noreply.github.com>
2021-05-31 07:36:34 -0400

    Update test_ise.m

--- test_ise.m
+++ test_ise.m
@@ -13,74 +13,77 @@ function [obj, varargout] = test_ise(savefig,varargin)
 %     fid = fopen([cwd '/cell_list_1pxFiltAll.txt'],'rt');
     cellList = textscan(fid,'%s','Delimiter','\n');
     cellList = cellList{1};
-% change directory
-for ss = 1:size(cellList,1)
-cellList(ss)=str2cell(strrep(cell2str(cellList(ss)),'Volumes','Users/yuhsuan'));
-end    
-
-% Make sure no empty cells
-notempty = ~cellfun(@isempty,cellList);
-cellList = cellList(notempty,:);
-% Generate unique identifier for each cell
-s = regexp(cellList{1},'session');
-identifiers = zeros(size(cellList,1),5);
-cellid = cell(size(cellList,1),1);
-missing = [];
-for ii = 1:size(cellList,1)
-    if exist(cellList{ii},'dir') == 7
-        % Collect date, session, array, channel, cell
-        identifiers(ii,:) = [str2double(cellList{ii}(s-9:s-2)) str2double(cellList{ii}(s+7:s+8)) ...
-            str2double(cellList{ii}(s+15:s+16)) str2double(cellList{ii}(s+25:s+27)) str2double(cellList{ii}(s+33:s+34))];
-        % Cell identifier
-        cellid{ii} = horzcat(num2str(identifiers(ii,4)),'-',num2str(identifiers(ii,5)));
-    else
-        missing = [missing ii];
-    end
-end
-% Remove missing cells
-identifiers(missing,:) = [];
-cellid(missing) = [];
-cellList(missing) = [];
-setsessions = unique(identifiers(:,1));
-% setcells = unique(cellid);
-    
-    
-% % % % % %     generate new vmsv
+% % change directory
 % for ss = 1:size(cellList,1)
-% 
-%     cd(strrep(cell2str(cellList(ss)),'Volumes','Users/yuhsuan'));%directory specific to my workstation
-%     sv=vmsv('auto');
-% %     get each cell's ise
-%     ise=[ise(:) sv.data.ISE];
-% %     get each cell's ise_threshold 95% percentile
-%     ise_thr =[ise_thr(:) prctile([sv.data.ISE; sv.data.ISEsh(:)],95)];
+% cellList(ss)=str2cell(strrep(cell2str(cellList(ss)),'Volumes','Users/yuhsuan'));
+% end    
+
+% % Make sure no empty cells
+% notempty = ~cellfun(@isempty,cellList);
+% cellList = cellList(notempty,:);
+% % Generate unique identifier for each cell
+% s = regexp(cellList{1},'session');
+% identifiers = zeros(size(cellList,1),5);
+% cellid = cell(size(cellList,1),1);
+% missing = [];
+% for ii = 1:size(cellList,1)
+%     if exist(cellList{ii},'dir') == 7
+%         % Collect date, session, array, channel, cell
+%         identifiers(ii,:) = [str2double(cellList{ii}(s-9:s-2)) str2double(cellList{ii}(s+7:s+8)) ...
+%             str2double(cellList{ii}(s+15:s+16)) str2double(cellList{ii}(s+25:s+27)) str2double(cellList{ii}(s+33:s+34))];
+%         % Cell identifier
+%         cellid{ii} = horzcat(num2str(identifiers(ii,4)),'-',num2str(identifiers(ii,5)));
+%     else
+%         missing = [missing ii];
+%     end
 % end
-
-% % % % % % % old
-% Load vmpv object for each session
-for ss = 1:size(setsessions,1)
-
-    cd(['/Users/yuhsuan/Hippocampus/Data/picasso-misc/' num2str(setsessions(ss)) '/session01']);
-    pv = load([num2str(pix) 'vmpv.mat']);
-    pv = pv.pv;
-    setcells = find(identifiers(:,1) == setsessions(ss));
-       
-    % Process placebyspatialview cell by cell
-    for cc = 1:size(setcells,1)
-        
-        cd(cellList{setcells(cc)});
-        disp(cellList{setcells(cc)});
-        sv=vmsv('auto');
-%       get each cell's ise
-        ise=[ise(:) sv.data.ISE];
-%       get each cell's ise_threshold 95% percentile
-        ise_thr =[ise_thr(:) prctile([sv.data.ISE; sv.data.ISEsh(:)],95)];
-   
-        close all;%from old file
-        
-    end
+% % Remove missing cells
+% identifiers(missing,:) = [];
+% cellid(missing) = [];
+% cellList(missing) = [];
+% setsessions = unique(identifiers(:,1));
+% % setcells = unique(cellid);
+%     
     
+% % % % %     generate new vmsv
+for ss = 1:size(cellList,1)
+    filename=strrep(cell2str(cellList(ss)),'Volumes','Users/yuhsuan');
+    if isfile(filename)
+        if isfile('spiketrain.mat')
+            cd(strrep(cell2str(cellList(ss)),'Volumes','Users/yuhsuan'));%directory specific to my workstation
+            sv=vmsv('auto');
+        %     get each cell's ise
+            ise=[ise(:) sv.data.ISE];
+        %     get each cell's ise_threshold 95% percentile
+            ise_thr =[ise_thr(:) prctile([sv.data.ISE; sv.data.ISEsh(:)],95)];
+        end
+    end
 end
+% % % % % % % % old
+% % Load vmpv object for each session
+% for ss = 1:size(setsessions,1)
+% 
+%     cd(['/Users/yuhsuan/Hippocampus/Data/picasso-misc/' num2str(setsessions(ss)) '/session01']);
+%     pv = load([num2str(pix) 'vmpv.mat']);
+%     pv = pv.pv;
+%     setcells = find(identifiers(:,1) == setsessions(ss));
+%        
+%     % Process placebyspatialview cell by cell
+%     for cc = 1:size(setcells,1)
+%         
+%         cd(cellList{setcells(cc)});
+%         disp(cellList{setcells(cc)});
+%         sv=vmsv('auto');
+% %       get each cell's ise
+%         ise=[ise(:) sv.data.ISE];
+% %       get each cell's ise_threshold 95% percentile
+%         ise_thr =[ise_thr(:) prctile([sv.data.ISE; sv.data.ISEsh(:)],95)];
+%    
+%         close all;%from old file
+%         
+%     end
+%     
+% end
 
 
 
