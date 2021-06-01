6e8e482eaa0c5ea73737b847acc2e18975b8d453
yuc125 <71300761+yuc125@users.noreply.github.com>
2021-05-31 21:42:01 -0400

    fix typo

--- @vmsv/vmsv.m
+++ @vmsv/vmsv.m
@@ -473,7 +473,7 @@ if(~isempty(dir(Args.RequiredFile)))
 
             %for testing
             data.actual_image=actual_image;
-            data.ashuffled_images=shuffled_images;
+            data.shuffled_images=shuffled_images;
 
             
             disp(['time taken to pad map for ISE: ' num2str(toc)]);
@@ -931,9 +931,9 @@ end
 % smoothedSpk(pos==0)=nan;
 % % report radii sizes?
 % 
-%  ISE=sv.data.ISE
+% ISE=sv.data.ISE
 % ISE_sh=sv.data.ISEsh;
 % ISE_thr=prctile([ISE;sv.data.ISEsh],95);
 % actual_image=sv.data.actual_image;
-% shuffled_images=sv.data.ashuffled_images;
+% shuffled_images=sv.data.shuffled_images;
 % save('/Users/yuhsuan//Desktop/test_ise.mat');
