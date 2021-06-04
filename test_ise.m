acf115f1dd27b7c91b09a4ed651cf6599d965997
yuc125 <71300761+yuc125@users.noreply.github.com>
2021-06-04 03:08:23 -0400

    Update test_ise.m

--- test_ise.m
+++ test_ise.m
@@ -27,17 +27,23 @@ for ii = 1:size(cellList,1)
         st=cellList{ii};
         array_p= strfind(st,'array');%position of 'array'
         filename= [st(1:array_p-1),'vmpv.mat'];%vmpv filename
+        filename2=strrep(cellList{ii},'/spiketrain.mat','vmsv_isechange.mat');
         if exist(filename,'file') == 2 
-            % Collect date, session, array, channel, cell
-            identifiers(ii,:) = [str2double(cellList{ii}(s-9:s-2)) str2double(cellList{ii}(s+7:s+8)) ...
-                str2double(cellList{ii}(s+15:s+16)) str2double(cellList{ii}(s+25:s+27)) str2double(cellList{ii}(s+33:s+34))];
-            % Cell identifier
-            cellid{ii} = horzcat(num2str(identifiers(ii,4)),'-',num2str(identifiers(ii,5)));
-        else
-        missing = [missing ii];
+            if exist(filename2,'file') == 2
+                % Collect date, session, array, channel, cell
+                identifiers(ii,:) = [str2double(cellList{ii}(s-9:s-2)) str2double(cellList{ii}(s+7:s+8)) ...
+                    str2double(cellList{ii}(s+15:s+16)) str2double(cellList{ii}(s+25:s+27)) str2double(cellList{ii}(s+33:s+34))];
+                % Cell identifier
+                cellid{ii} = horzcat(num2str(identifiers(ii,4)),'-',num2str(identifiers(ii,5)));
+            else
+                missing = [missing ii];
+            end
         end
     end
 end
+% remove already genreated vmsv.m
+
+
 % Remove missing cells
 identifiers(missing,:) = [];
 cellid(missing) = [];
@@ -45,10 +51,6 @@ cellList(missing) = [];
 setsessions = unique(identifiers(:,1));
 % setcells = unique(cellid);
 
-% %use identifier to make file name tag
-% for n=1:size(identifiers,1)
-%     trail_name=[trail_name ;[string(identifiers(n,1)), '-session',string(identifiers(n,2)),'array',string(identifiers(n,3)),'channel',string(identifiers(n,4)),'cell',string(identifiers(n,5))];
-% end
 
 % Load vmpv object for each session
 ise=[];
@@ -59,22 +61,24 @@ for ss = 1:size(cellList,1)
     cd(strrep(cellList{ss},'/spiketrain.mat',''));
 %     cd ..\ %up one to cell01
     sv=vmsv('auto');
-%     get each cell's ise
-    ise=[ise; sv.data.ISE];
-%     get each cell's ise_threshold 95% percentile
-    ise_thr =[ise_thr; prctile([sv.data.ISE; sv.data.ISEsh(:)],95)];
+    save('vmsv_isechange.mat','sv');
+%     load()
+% %     get each cell's ise
+%     ise=[ise; sv.data.ISE];
+% %     get each cell's ise_threshold 95% percentile
+%     ise_thr =[ise_thr; prctile([sv.data.ISE; sv.data.ISEsh(:)],95)];
 end
-% average the each cell's ise threshold to get overall threshold
-    ise_thr_avg=mean(ise_thr(:));
-% find the difference between the ise and overall threshold
-%     each cell's ise-overall threshold
-       difference=ise-ise_thr_avg;
-%     each cell's ise/overall threshold
-        ratio=ise/ise_thr_avg;
-%     ise_result=[identifiers ise ise_thr difference ratio];
-%  save('/Users/yuhsuan/Desktop/test_list2ise_ted.mat');
-time=toc;
- save('/Users/yuhsuan/Desktop/test_list2ise.mat','identifiers','ise','ise_thr','ise_thr_avg','difference','ratio','time');
+% % average the each cell's ise threshold to get overall threshold
+%     ise_thr_avg=mean(ise_thr(:));
+% % find the difference between the ise and overall threshold
+% %     each cell's ise-overall threshold
+%        difference=ise-ise_thr_avg;
+% %     each cell's ise/overall threshold
+%         ratio=ise/ise_thr_avg;
+% %     ise_result=[identifiers ise ise_thr difference ratio];
+% %  save('/Users/yuhsuan/Desktop/test_list2ise_ted.mat');
+% time=toc;
+%  save('/Users/yuhsuan/Desktop/test_list2ise.mat','identifiers','ise','ise_thr','ise_thr_avg','difference','ratio','time');
 end
 
 
