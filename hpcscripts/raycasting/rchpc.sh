74789dc919842e8ec8660b31c5b83c52992f9921
xavierpok <54708640+xavierpok@users.noreply.github.com>
2024-03-03 11:50:02 +0800

    Update & add binning scripts

--- hpcscripts/raycasting/rchpc.sh
+++ hpcscripts/raycasting/rchpc.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
+#set -x
 
 rad=$1
-cat ../sessions.txt | while read line
-do
-	temp="${line##*Data}/session01"
-	echo Reading from sessions.txt: $line
-	line+="/session01"
-	dirpath=~/hpctmp/Data$temp
-	mkdir -p $dirpath
-	echo hpc session directory: $dirpath
-	for file in unityfile.mat eyelink.mat
-	do
-		if [ ! -f $dirpath/$file ]; then
-			scp hippocampus@cortex.nus.edu.sg:$line/$file $dirpath 
-		fi
-	done
-	curr=$(pwd)
-	cd $dirpath
-	echo $dirpath > batch.txt
-		rcjob=$(qsub -v rad=$rad $curr/rcsubmit.pbs)
-		qsub -W depend=afterok:$rcjob -v rad=$rad $curr/rctrf.pbs
-	echo Changing back to working dir: $curr
-	cd $curr	
-done
+while IFS= read -r line || [ -n "$line" ]; do
+    temp="${line##*Data}/session01"
+    echo "Reading from sessions.txt: $line"
+    line+="/session01"
+    dirpath=~/hpctmp/Data$temp
+    mkdir -p "$dirpath"
+    echo "---"
+    echo "hpc session directory: $dirpath"
+    for file in unityfile.mat eyelink.mat; do
+        if [ ! -f "$dirpath/$file" ]; then
+            scp "hippocampus@cortex.nus.edu.sg:$line/$file" "$dirpath"
+        fi
+    done
+
+    curr=$(pwd)
+    cd "$dirpath"
+    echo "$dirpath" > batch.txt
+    rcjob=$(qsub -v rad="$rad" "$curr/rcsubmit.pbs")
+    qsub -W depend=afterok:"$rcjob" -v rad="$rad" "$curr/rctrf.pbs"
+    binnerjob=$(qsub -W depend=afterok:"$rcjob" -v curr="$curr" "$curr/binning.pbs")
+
+    echo "Changing back to working dir: $curr"
+    cd "$curr"
+done < ../sessions.txt
 
--- hpcscripts/raycasting/transfer.sh
+++ hpcscripts/raycasting/transfer.sh
@@ -6,6 +6,7 @@ cat sessions.txt | while read line
 do
 	temp=${line##*Data}
 	echo Transferring files from: ~/hpctmp/Data$temp
+
 	cd ~/hpctmp/Data$temp
 	scp ${rad}binData.csv hippocampus@cortex.nus.edu.sg:$line/${rad}binData.csv
 	scp unityfile_eyelink.csv logs.txt VirtualMazeBatchLog.txt hippocampus@cortex.nus.edu.sg:$line/ 
