a82dea2a869ddc7e03dc4e301b868adc2563c040
Joseph Cheng <59868375+josephchengyx@users.noreply.github.com>
2024-03-04 17:04:25 +0800

    Update hpcscripts
    Fix file transfer scripts

--- hpcscripts/vmpv_hpc/transfer.sh
+++ hpcscripts/vmpv_hpc/transfer.sh
@@ -4,10 +4,10 @@ curr=$(pwd)
 rad=$1
 cat sessions.txt | while read line
 do
-	temp=${line##*Data}
-	echo Transferring files from: ~/hpctmp/Data$temp
-	cd ~/hpctmp/Data$temp
-	scp vmpv.mat hippocampus@cortex.nus.edu.sg:$line/${rad}vmpv.mat 
+	temp=${line##*picasso-misc}
+	echo Transferring files from: ~/hpctmp/Data/picasso-misc$temp
+	cd ~/hpctmp/Data/picasso-misc$temp
+	scp vmpv.mat hippocampus@cortex.nus.edu.sg:/picasso-misc-folder-link$temp/${rad}vmpv.mat 
 done
 cd $curr
 
