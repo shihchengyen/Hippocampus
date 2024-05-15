cd47800f07b0691df9d80960807f552b4d7879f2
xavierpok <54708640+xavierpok@users.noreply.github.com>
2024-05-15 21:13:32 +0800

    Add processing scripts for multiple radii

--- hpcscripts/vmsv_hpc/vmshpc.sh
+++ hpcscripts/vmsv_hpc/vmshpc.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+rad=$1
+rad_str=$(echo "$rad" | awk '{gsub(/\./,"-",$1); print $1}')
+prefix="r${rad_str}"
+vmpv_name="${prefix}vmpv.mat"
+
 cat cells.txt | while read line
 do
 	celldir=${line##*Data}
@@ -8,6 +13,7 @@ do
 	echo hpc cell directory: ~/hpctmp/Data$celldir
     spiketrainFILE=~/hpctmp/Data"$celldir"/spiketrain.mat
     if [ ! -f $spiketrainFILE ]; then
+	echo "Copying spiketrain from hippocampus"
         scp hippocampus@cortex.nus.edu.sg:$line/spiketrain.mat ~/hpctmp/Data$celldir
     fi
     
@@ -16,14 +22,20 @@ do
     mkdir -p ~/hpctmp/Data$sessdir
     echo hippocampus session directory: $hippsessdir
     echo hpc session directory: ~/hpctmp/Data$sessdir
-    vmpvFILE=~/hpctmp/Data"$sessdir"/vmpv.mat
+    vmpvFILE="${HOME}/hpctmp/Data${sessdir}/${vmpv_name}"
+    echo vmpvfile : $vmpvFILE 
     if [ ! -f $vmpvFILE ]; then
-        scp hippocampus@cortex.nus.edu.sg:$hippsessdir/1vmpv.mat ~/hpctmp/Data$sessdir/vmpv.mat
+	
+        scp hippocampus@cortex.nus.edu.sg:"${hippsessdir}/${vmpv_name}" "${vmpvFILE}"
     fi
     
     curr=$(pwd)
     cd ~/hpctmp/Data$celldir
-        qsub $curr/vmssubmit.txt
+    mvms=$(qsub -v prefix="$prefix" $curr/vmssubmit.pbs)
+    singlevms=$(qsub -v prefix="" $curr/vmssubmit.pbs)
+    #echo "qsub -v prefix="$prefix" -W dependon=afterok:"$mvms" "$curr/plotvms.pbs""
+    mplot=$(qsub -v prefix="$prefix" -W depend=afterok:"$mvms" "$curr/plotvms.pbs")
+    singleplot=$(qsub -v prefix="" -W depend=afterok:"$singlevms" "$curr/plotvms.pbs")
     echo Changing back to working dir: $curr
     cd $curr
     echo ~/hpctmp/Data$celldir > vmsvbatch.txt
---
+++ hpcscripts/working.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#set -x
+
+rad=$1
+rad_str=$(echo "$rad" | awk '{gsub(/\./,"-",$1); print $1}')
+multi_output_file="unityfile_eyelink_r${rad_str}.csv"
+multi_save_file="r${rad_str}binData.csv"
+while IFS= read -r line || [ -n "$line" ]; do
+    temp="${line##*Data}/session01"
+    echo "Reading from sessions.txt: $line"
+    line+="/session01"
+    dirpath=~/hpctmp/Data$temp
+    mkdir -p "$dirpath"
+    echo "---"
+    echo "hpc session directory: $dirpath"
+    for file in unityfile.mat eyelink.mat; do
+        if [ ! -f "$dirpath/$file" ]; then
+            scp "hippocampus@cortex.nus.edu.sg:$line/$file" "$dirpath"
+        fi
+    done
+
+    curr=$(pwd)
+    cd "$dirpath"
+    echo "$dirpath" > batch.txt
+    #echo "qsub -v rad="$rad" "$curr/raycasting/rcsubmit.pbs" -N "${line}_${multi_output_file}_raycast""
+    rcjob=$(qsub -v rad="$rad" -N "${rad}_raycast" "$curr/raycasting/rcsubmit.pbs" )
+    echo $rcjob
+    qsub -W depend=afterok:"$rcjob" -v rad="$rad" "$curr/raycasting/rctrf.pbs"
+    echo "curr : $curr"
+    binnerjob=$(qsub -W depend=afterok:"$rcjob"  -N "${rad}_bin" -v curr="$curr",single_path="unityfile_eyelink.csv",single_save_path="1binData.csv",multi_path="$multi_output_file",multi_save_path="$multi_save_file"  "$curr/raycasting/binning.pbs")
+
+    echo "Changing back to working dir: $curr"
+    cd "$curr"
+
+    echo "Setting up vmpv"
+    pvjob1=$(qsub -v file_name=1binData.csv -W depend=afterok:"$binnerjob"  -N "1_pv" "$curr/vmpv_hpc/pvsubmit.pbs")
+    qsub -W depend=afterok:"$pvjob1" -v rad="$rad" "$curr/vmpv_hpc/pvtrf.pbs"
+    echo "Setting vmpv ${multi_output_file}"
+    pvjobm=$(qsub -v file_name=$multi_save_file -W depend=afterok:"$binnerjob" -N "${rad}_pv" "$curr/vmpv_hpc/pvsubmit.pbs")
+    qsub -W depend=afterok:"$pvjobm" -v rad="$rad" "$curr/vmpv_hpc/pvtrf.pbs"
+    echo Changing back to working dir: "$curr"
+    cd "$curr"
+
+
+    rad_str=$(echo "$rad" | awk '{gsub(/\./,"-",$1); print $1}')
+    prefix="r${rad_str}"
+    vmpv_name="${prefix}vmpv.mat"
+    
+    echo "Doing vmsv"
+    $curr/get_all_cells.sh "$line" "hippocampus@cortex.nus.edu.sg" | while IFS= read -r cell; do
+        celldir=${cell##*Data}
+        mkdir -p ~/hpctmp/Data"$celldir"
+        
+        spiketrainFILE=~hpctmp/Data"$celldir"/spiketrain.mat
+        if [ ! -f $spiketrainFILE ]; then
+	    echo "Copying spiketrain from hippocampus"
+            scp hippocampus@cortex.nus.edu.sg:$cell/spiketrain.mat ~/hpctmp/Data$celldir
+        fi
+        cd ~/hpctmp/Data$celldir
+        echo "${cell}_${prefix}vms"
+        echo "PV job : $pvjobm , $pvjob1" 
+        mvms=$(qsub -v prefix="$prefix" -N "${prefix}vms" -W depend=afterok:"$pvjobm" $curr/vmsv_hpc/vmssubmit.pbs) 
+        singlevms=$(qsub -v prefix="" -N "1vms" -W depend=afterok:"$pvjob1" $curr/vmsv_hpc/vmssubmit.pbs) 
+        mplot=$(qsub -v prefix="$prefix" -N "${prefix}plot" -W depend=afterok:"$mvms" "$curr/vmsv_hpc/plotvms.pbs")
+        singleplot=$(qsub -v prefix="" -N "singleplot" -W depend=afterok:"$singlevms" "$curr/vmsv_hpc/plotvms.pbs")
+        echo Changing back to working dir: $curr
+        cd $curr
+     done
+
+done < sessions.txt
+
