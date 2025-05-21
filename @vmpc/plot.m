75a845b859984d6ae1fef71ce97bf0e2cc3292af
Roger Heirkstad <roger.herikstad@gmail.com>
2025-05-21 09:20:28 +0800

    Update plot function for vmpc from master

--- @vmpc/plot.m
+++ @vmpc/plot.m
@@ -17,9 +17,123 @@ Args = struct('LabelsOff',0,'GroupPlots',1,'GroupPlotIndex',1,'Color','b', ...
 		  'ReturnVars',{''}, 'ArgsOnly',0, 'Cmds','', 'Errorbar',0, ...
           'Shuffle',0, 'ShuffleSteps',100, 'NumSubPlots',4, ...
           'Map',0,'Smooth',1,'SIC',0,'Radii',0,'MinDur',0,'Filtered',1,...
-          'SortByRatio',0,'Details',1,'RateBins',0,'MapOnly',0);
-Args.flags = {'LabelsOff','ArgsOnly','Errorbar','SIC','Shuffle','MapOnly'};
+          'SortByRatio',0,'Details',1,'RateBins',0,'MapOnly',0,'plotmap',0);
+Args.flags = {'LabelsOff','ArgsOnly','Errorbar','SIC','Shuffle','MapOnly','plotmap'};
 [Args,varargin2] = getOptArgs(varargin,Args);
+
+if Args.plotmap
+% Insert floor place map into larger 3D view setting
+    
+    mapL = obj.data.maps_adsm;
+    ax = gca;
+    mapLtemp = mapL;
+    mapL = nan(1,5122);
+    mapL(3:3+1600-1) = mapLtemp;
+    mapG = flipud(reshape(mapLtemp, 40, 40)');
+    mapGdummy = flipud(reshape(1:1600, 40, 40)');
+       
+    
+    mapLdummy = 1:length(mapL);
+    % Set up surf frame for plotting
+    floor_x = repmat(0:40, 41, 1);
+    floor_y = flipud(repmat([0:40]', 1, 41));
+    floor_z = zeros(41,41);
+
+    ceiling_x = floor_x;
+    ceiling_y = floor_y;
+    ceiling_z = 40.*ones(41,41);
+
+    walls_x = repmat([0.*ones(1,40) 0:39 40.*ones(1,40) 40:-1:0], 9, 1);
+    walls_y = repmat([0:39 40.*ones(1,40) 40:-1:1 0.*ones(1,41)], 9, 1);
+    walls_z = repmat([24:-1:16]', 1, 40*4 + 1);
+
+    P1_x = repmat([24.*ones(1,8) 24:31 32.*ones(1,8) 32:-1:24], 6, 1);
+    P1_y = repmat([8:15 16.*ones(1,8) 16:-1:9 8.*ones(1,9)], 6, 1);
+    PX_z = repmat([21:-1:16]', 1, 8*4 + 1);
+
+    P2_x = repmat([8.*ones(1,8) 8:15 16.*ones(1,8) 16:-1:8], 6, 1);
+    P2_y = P1_y;
+
+    P3_x = P1_x;
+    P3_y = repmat([24:31 32.*ones(1,8) 32:-1:25 24.*ones(1,9)], 6, 1);
+
+    P4_x = P2_x;
+    P4_y = P3_y;
+
+    floor = flipud(reshape(mapL(3:3+1600-1), 40, 40)');
+    floordum = flipud(reshape(mapLdummy(3:3+1600-1), 40, 40)');
+
+    % ceiling follows floor mapping, top down view
+    ceiling = flipud(reshape(mapL(1603:1603+1600-1), 40, 40)');
+    ceilingdum = flipud(reshape(mapLdummy(1603:1603+1600-1), 40, 40)');
+
+    % from top down, slit walls at bottom left corner, open outwards.
+    % start from row closest to ground, rightwards, then climb rows
+    walls = flipud(reshape(mapL(3203:3203+1280-1), 40*4, 8)');
+    wallsdum = flipud(reshape(mapLdummy(3203:3203+1280-1), 40*4, 8)');
+
+    % BL - bottom left, and so on, from top view, same slicing as walls
+    % pillar width 8, height 5
+    P1_BR = flipud(reshape(mapL(4483:4483+160-1), 8*4, 5)');
+    P1_BRdum = flipud(reshape(mapLdummy(4483:4483+160-1), 8*4, 5)');
+    P2_BL = flipud(reshape(mapL(4643:4643+160-1), 8*4, 5)');
+    P2_BLdum = flipud(reshape(mapLdummy(4643:4643+160-1), 8*4, 5)');
+    P3_TR = flipud(reshape(mapL(4803:4803+160-1), 8*4, 5)');
+    P3_TRdum = flipud(reshape(mapLdummy(4803:4803+160-1), 8*4, 5)');
+    P4_TL = flipud(reshape(mapL(4963:4963+160-1), 8*4, 5)');
+    P4_TLdum = flipud(reshape(mapLdummy(4963:4963+160-1), 8*4, 5)');
+    
+ % Pad with NaNs for surf plots
+    P1_BR = [P1_BR; nan(1,size(P1_BR,2))];
+    P1_BR = [P1_BR nan(size(P1_BR,1),1)];
+
+    P2_BL = [P2_BL; nan(1,size(P2_BL,2))];
+    P2_BL = [P2_BL nan(size(P2_BL,1),1)];        
+
+    P3_TR = [P3_TR; nan(1,size(P3_TR,2))];
+    P3_TR = [P3_TR nan(size(P3_TR,1),1)];                
+
+    P4_TL = [P4_TL; nan(1,size(P4_TL,2))];
+    P4_TL = [P4_TL nan(size(P4_TL,1),1)];
+
+    % Plot floor
+    surf(floor_x, floor_y, floor_z, floor);
+    alpha 1; shading flat;
+    hold on;
+
+    % Plot ceiling and walls
+    surf(ceiling_x, ceiling_y, ceiling_z, ceiling);
+    alpha 1; shading flat;
+    surf(walls_x, walls_y, walls_z, walls);      
+    alpha 1; shading flat;
+
+    % Plot pillars
+    surf(P1_x, P1_y, PX_z, P1_BR);
+    alpha 1; shading flat;
+    surf(P2_x, P2_y, PX_z, P2_BL);
+    alpha 1; shading flat;
+    surf(P3_x, P3_y, PX_z, P3_TR);
+    alpha 1; shading flat;
+    surf(P4_x, P4_y, PX_z, P4_TL);
+    alpha 1; shading flat;
+
+    axlim = max(abs([get(ax, 'xlim'), get(ax, 'ylim'),get(ax, 'zlim')])); % Make axes square around biggest value
+    axis(ax,[0 axlim 0 axlim 0 axlim]);  
+    
+ 
+    colormap jet;
+    
+    alpha 1; shading flat; 
+    view(-35,20);
+    
+    axis(ax,'tight');
+    w = ax.OuterPosition(3)*0.02;
+    x = ax.OuterPosition(1) + ax.OuterPosition(3);
+    y = ax.OuterPosition(2) + ax.OuterPosition(4)/4;
+    h = ax.OuterPosition(4)/2;
+
+    c = colorbar(ax,'Position',[x y w h]);
+end
 
 % if user select 'ArgsOnly', return only Args structure for an empty object
 if Args.ArgsOnly
@@ -31,6 +145,8 @@ end
 if(~isempty(Args.NumericArguments))
 	% plot one data set at a time
 % 	n = get(gcf,'UserData');
+
+    colormap jet;
 
     po = findobj(gcf,'String','Plot Options');
     set(po, 'Visible', 'off');
